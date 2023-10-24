fdad9f25b97aaa16d16f42390c271a90079477c9
Mark Junglas <90196835+Schmarsch@users.noreply.github.com>
2023-10-24 14:55:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,80 +1,85 @@
-const core = require("@actions/core")
-const github = require("@actions/github")
-const fs = require("fs")
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
 
-async function checkForFileExists(filePath) {
-  return fs.promises
-    .access(filePath)
+
+async function checkFileExists(filePath) {
+    return fs.promises.access(filePath)
     .then(() => {
-      core.info(`File ${filePath} exists`)
-      return true
+        core.info(`File ${filePath} exists`);
+        return true;
     })
     .catch(() => {
-      core.setFailed(`File ${filePath} does not exist but is mandatory`)
-      return false
-    })
+        core.setFailed(`File ${filePath} is mandatory`);
+        return false;
+    });
 }
 
-// create a function that check if the file starts with a markdown header
-async function checkForMarkdownHeader(filePath) {
-  return fs.promises
-    .readFile(filePath, "utf8")
-    .then((data) => {
-      // remove all the empty at the beginning of the file using a regex
-      data = data.replace(/^\s*\n/gm, "")
-      if (data.startsWith("# ")) {
-        core.info(`File ${filePath} starts with a markdown header`)
-        return true
-      } else {
-        core.setFailed(`File ${filePath} does not start with a markdown header`)
-        return false
-      }
-    })
-    .catch(() => {
-      core.setFailed(`File ${filePath} does not exist but is mandatory`)
-      return false
-    })
+// create a function that checks if the file starts with a markdown header
+async function checkFileStartsWithHeader(filePath) {
+    return fs.promises.readFile(filePath, 'utf8')
+    .then(fileContent => {
+
+        // remove all empty lines ad the beginning of the file
+        fileContent = fileContent.replace(/^\s*\n/gm, '');
+
+        if (fileContent.startsWith('#')) {
+            core.info(`File ${filePath} starts with a header`);
+            return true;
+        } else {
+            core.setFailed(`File ${filePath} does not start with a header`);
+            return false;
+        }
+    });
 }
 
-;(async () => {
-  try {
-    checkForFileExists("LICENSE")
-    checkForFileExists("README.md")
+(
+    async () => {
+        try {
+            checkFileExists("LICENSE");
+            checkFileExists("README.md");
 
-    if (!(await checkForMarkdownHeader("README.md"))) {
-      // get token for octokit
-      const token = core.getInput("repo-token")
-      const octokit = github.getOctokit(token)
+            if (
+                ! await checkFileStartsWithHeader("README.md")
+            ) {
+                // get token for octokit
+                const token = core.getInput('repo-token');
+                const octokit = new github.getOctokit(token);
 
-	core.notice("Creating a check for README.md")
+                // call octokit to create a check with annotation and details
+                const check = await octokit.rest.checks.create({
+                    owner: github.context.repo.owner,
+                    repo: github.context.repo.repo,
+                    name: 'Readme Validator',
+                    head_sha: github.context.sha,
+                    status: 'completed',
+                    conclusion: 'failure',
+                    output: {
+                        title: 'README.md must start with a title',
+                        summary: 'Please use markdown syntax to create a title',
+                        annotations: [
+                            {
 
-      // call octokit to create a check with annotations and details
-      const check = await octokit.rest.checks.create({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        name: "Readme Validator",
-        head_sha: github.context.sha,
-        status: "completed",
-        conclusion: "failure",
-        output: {
-          title: "README.md must start with a title",
-          summary:
-            "Please use markdown syntax to add a title ad the beginning of your README.md file",
-          annotations: [
-            {
-              path: "README.md",
-              start_line: 1,
-              end_line: 1,
-              annotation_level: "failure",
-              message: "README.md must start with a header",
-              start_column: 1,
-              end_column: 1,
-            },
-          ],
-        },
-      })
+                                path: 'README.md',
+                                start_line: 1,
+                                end_line: 1,
+                                annotation_level: 'failure',
+                                message: 'README.md must start with a header',
+                                start_column: 1,
+                                end_column: 1
+                            }
+                        ]
+                    }
+                });
+
+
+
+            }
+            
+
+        } catch (error) {
+            core.setFailed(error.message);
+        }
     }
-  } catch (error) {
-    core.setFailed(error.message)
-  }
-})()
+
+)();
