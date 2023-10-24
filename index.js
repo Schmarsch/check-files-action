bb4749983ae94d1b3a8805b57ccc4c0a6fde9a9a
Mark Junglas <m.junglas@wzl.rwth-aachen.de>
2023-10-24 14:26:06 +0200

    first tests for title

--- index.js
+++ index.js
@@ -15,10 +15,36 @@ async function checkForFileExists(filePath) {
     })
 }
 
+// create a function that check if the file starts with a markdown header
+async function checkForMarkdownHeader(filePath) {
+	return fs.promises
+		.readFile(filePath, "utf8")
+		.then((data) => {
+			if (data.startsWith("# ")) {
+
+				// remove all the empty at the beginning of the file using a regex
+				data = data.replace(/^\s*\n/gm, "")
+
+				core.info(`File ${filePath} starts with a markdown header`)
+				return true
+			} else {
+				core.setFailed(`File ${filePath} does not start with a markdown header`)
+				return false
+			}
+		})
+		.catch(() => {
+			core.setFailed(`File ${filePath} does not exist but is mandatory`)
+			return false
+		})
+}
+
 ;(async () => {
   try {
 		checkForFileExists("LICENSE")
 		checkForFileExists("README.md")
+
+		checkForMarkdownHeader("README.md")
+
   } catch (error) {
     core.setFailed(error.message)
   }
