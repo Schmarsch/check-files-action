cb8863ae73d1f6c9c3aa137f88f726888597e9de
Mark Junglas <m.junglas@wzl.rwth-aachen.de>
2023-10-24 14:21:00 +0200

    add check files func

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core")
+const github = require("@actions/github")
+const fs = require("fs")
 
-(
-	async () => {
-		try {
-			core.notice("Calling my action")
-		} catch(error) {
-			core.setFailed(error.message);
-		}
-	}
-)();
+async function checkForFileExists(filePath) {
+  return fs.promises
+    .access(filePath)
+    .then(() => {
+      core.info(`File ${filePath} exists`)
+      return true
+    })
+    .catch(() => {
+      core.setFailed(`File ${filePath} does not exist but is mandatory`)
+      return false
+    })
+}
+
+;(async () => {
+  try {
+		checkForFileExists("LICENSE")
+		checkForFileExists("README.md")
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+})()
